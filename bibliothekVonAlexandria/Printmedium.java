a6e97dcabd1fa87d3cb30b83143e9884f4361364
minaolensarah <44339052+minaolensarah@users.noreply.github.com>
2019-01-09 22:23:00 +0100

    Update Printmedium.java

--- bibliothekVonAlexandria/Printmedium.java
+++ bibliothekVonAlexandria/Printmedium.java
@@ -1,7 +1,9 @@
 package bibliothekVonAlexandria;
 
 /**
- * Ein Printmedium hat einen Titel, eine Seitenzahl und es ist entweder entliehen oder nicht. Dieser Status kann verändert werden, dabei wird gespeichert von wem und bis wann das Medium entliehen wurde.
+ * Ein Printmedium hat einen Titel, eine Seitenzahl und es ist entweder entliehen oder nicht. 
+ * Dieser Status kann verändert werden, dabei wird gespeichert von wem und bis wann das Medium entliehen wurde.
+ * Es kann vom Typ Buch, Paper, Journal oder Zeitung sein.
  * 
  * @author Sarah Breckner 
  * @author Kim Lingemann
@@ -9,10 +11,21 @@ package bibliothekVonAlexandria;
  */
 class Printmedium extends Medien {
 	private final int pages;
+	private final String typ;
 	
-	public Printmedium(final int pages1, final String title1) {
+	public Printmedium(final int pages1, final String title1, final String typ1) {
 		super(title1);
-		pages=pages1;		
+		pages=pages1;
+		typ = typ1;
+	}
+	/**
+	  * Der Typ des Printmediums wird abgefragt
+	  * 
+	  * @return typ, Typ des Printmediums
+	  * @requires Medium ist initialisiert 
+	  */
+	private String getTyp() {
+		return typ;
 	}
 	
 	/**
